a44282ba9591e72d04947b1a04f9e5b65548f0c6
Sondre Grønås <44143748+sondregronas@users.noreply.github.com>
2022-08-13 21:22:25 +0200

    Reformat footermatter

--- resources/assets/js/mkdocs-to-lightbox.js
+++ resources/assets/js/mkdocs-to-lightbox.js
@@ -1,4 +1,4 @@
-Array.prototype.forEach.call(document.body.querySelectorAll('.md-content img:not(.contributor-image)'), e => {
+Array.prototype.forEach.call(document.body.querySelectorAll('.md-content img:not(.footermatter-author)'), e => {
   let lightbox = document.createElement('a');
   lightbox.setAttribute('data-fslightbox', '');
   lightbox.setAttribute('href', e.src);
