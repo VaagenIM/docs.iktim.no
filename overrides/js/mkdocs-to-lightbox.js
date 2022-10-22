61577d6ebd3e59f1a697836657ffaa393900a02a
Sondre Grønås <44143748+sondregronas@users.noreply.github.com>
2022-10-22 23:01:31 +0200

    :wrench: Add glightbox

--- overrides/js/mkdocs-to-lightbox.js
+++
@@ -1,7 +0,0 @@
-Array.prototype.forEach.call(document.body.querySelectorAll('.md-content img:not(.footermatter-author)'), e => {
-  let lightbox = document.createElement('a');
-  lightbox.setAttribute('data-fslightbox', '');
-  lightbox.setAttribute('href', e.src);
-  lightbox.innerHTML = `<img src=${e.src}>`;
-  e.parentNode.replaceChild(lightbox, e);
-});
